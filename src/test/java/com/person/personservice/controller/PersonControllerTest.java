45e17c9bd2a629009635e7dc527f7620b957558c
Jangleur1991 <Jangleur@web.de>
2024-04-26 06:05:18 +0100

    fix testThatGetReturnsAllPerson

--- src/test/java/com/person/personservice/controller/PersonControllerTest.java
+++ src/test/java/com/person/personservice/controller/PersonControllerTest.java
@@ -3,22 +3,25 @@ package com.person.personservice.controller;
 import com.person.personservice.model.PersonDTO;
 import com.person.personservice.service.PersonService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.*;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.LinkedHashSet;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @WebMvcTest
 class PersonControllerTest {
@@ -42,7 +45,7 @@ class PersonControllerTest {
                 pageable,
                 1
         );
-        given(personService.getAllPerson(pageable)).willReturn(personPage);
+        given(personService.getAllPerson(any(Pageable.class))).willReturn(personPage);
 
         //when / then
         mockMvc.perform(get("/person"))
@@ -51,6 +54,13 @@ class PersonControllerTest {
                 .andExpect(jsonPath("$.content[0].name").value("testperson"))
                 .andExpect(jsonPath("$.content[0].parentIds").isEmpty());
 
+        ArgumentCaptor<Pageable> pageableCaptor = ArgumentCaptor.forClass(Pageable.class);
+        verify(personService).getAllPerson(pageableCaptor.capture());
+
+        PageRequest pageRequest = (PageRequest) pageableCaptor.getValue();
+        assertThat(pageRequest.getPageNumber()).isEqualTo(0);
+        assertThat(pageRequest.getPageSize()).isEqualTo(20);
+        assertThat(pageRequest.getSort()).isEqualTo(Sort.unsorted());
     }
 
 
